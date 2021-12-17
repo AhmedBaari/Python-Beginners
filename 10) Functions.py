fc1c46f38b2e2d5d2a8e6530ba39841754acd7bb
Ahmed <ahmed4baari@gmail.com>
2021-12-17 08:38:28 +0530

    Further Changes for Topics 8-10

--- 10) Functions.py
+++ 10) Functions.py
@@ -1,14 +1,20 @@
-#define
+'''
+FUNCTIONS: ONE OF THE MOST IMPORTANT PARTS OF PYTHON
+A Python function is a group of code that can be called from anywhere after it is defined. Functions can return a value using a return statement.
+ Function blocks begin with the keyword def followed by the function name and parentheses ( ( ) ). Any input parameters or arguments should be placed within these parentheses. You can also define parameters inside these parentheses.
+'''
+#Defining a python
 def say_hi():
     print("Hello User")
+#It won't run until it is called using say_hi()
 
-print("top") #comes 1st
-say_hi() #calling say hi, comes 2nd
-print("bottom") #comes 3rd
+print("top") #Comes 1st
+say_hi() #Hello User #Calling say_hi, comes 2nd
+print("bottom") #Comes 3rd
 
-def heya(name, age):
+def heya(name, age): # 2 variables used here
     print("Hello " + name + "! You are " + age + " years old.")
 
-name = input("Your name: ")
-age = input("Your age: ")
-heya(name, age)
+name = input("Your name: ") #First Variable
+age = input("Your age: ") #Second Variable
+heya(name, age) #Calling function using the 2 variables
--- 9) Tuples.py
+++ 9) Tuples.py
@@ -1,7 +1,9 @@
-#Most common is co-ordinates
+#Tuples are mostly used for co-ordinates
 coordinates = (4,5)
+
 #tuples are immutable, they cannot be modified.
 print(coordinates[0]) #4
 print(coordinates[1]) #5
 
+#Tuples can be used inside lists
 pallete = [(4,5), (6,7), (80,34)]
